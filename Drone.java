2013a29428aec5b53a81b3a2da6ee2afa2eecdc4
CathyWrHua <cathy.hua@gmail.com>
2017-01-07 23:48:41 -0500

    Update Drone.java

--- Drone.java
+++ Drone.java
@@ -1,17 +1,17 @@
+//Author: Cathy Hua
+//Date: January 8, 2017
+//Description: Drone class 
 
 public class Drone extends FlyingThing{
 	private float gpaDeduction;
 	
+	//default constructor with additional gpaDeduction value
 	Drone(){
 		super();
-		gpaDeduction = 0;
-	}
-	
-	public void kill(){
-		isStillAlive = false;
 		gpaDeduction = 1.0f;
 	}
 	
+	//returns GpaDeduction value for drone
 	public float getGpaDeduction() {
 		return gpaDeduction;
 	}
