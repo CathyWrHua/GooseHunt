3678ddfda915db5438939ed607e453eb9acbba43
CathyWrHua <cathy.hua@gmail.com>
2017-01-07 22:58:42 -0500

    Update Drone.java

--- Drone.java
+++ Drone.java
@@ -7,23 +7,13 @@ public class Drone extends FlyingThing{
 		gpaDeduction = 0;
 	}
 	
-	public void update(){
-		changeX();
-		changeY();
-	
-		if (lifetime == 0){
-			isFlying = false;
-		}
-	}
-	
-	public void setIsStillAlive(boolean isShot){
-		if (isShot){
-			isStillAlive = false;
-			gpaDeduction = 1.0f;
-		}
+	public void kill(){
+		isStillAlive = false;
+		gpaDeduction = 1.0f;
 	}
 	
 	public float getGpaDeduction() {
 		return gpaDeduction;
 	}
+	
 }
